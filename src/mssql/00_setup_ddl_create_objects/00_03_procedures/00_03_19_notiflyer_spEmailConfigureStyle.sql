bc52ab4d8e3d72c05ed5ea1cf952cce29567488d
cleancoda <pranav.shirodkar@protonmail.com>
2023-12-12 07:41:28 -0500

    kicking off #70 - emails!

---
+++ src/mssql/00_setup_ddl_create_objects/00_03_procedures/00_03_19_notiflyer_spEmailConfigureStyle.sql
@@ -0,0 +1,26 @@
+if object_id('notiflyer_spEmailConfigureStyle') is not null
+    print 'notiflyer_spEmailConfigureStyle stored procedure exists, skipping create attempt..'
+    print 'ignore error below, cannot create exception handling on ddl statements'
+    return;
+go
+
+-- drop procedure notiflyer_spEmailConfigureStyle
+create procedure notiflyer_spEmailConfigureStyle
+/*
+    @author     cleancoda
+    @date       12122023
+    @detail     
+    @sample
+                declare
+                    @returnvalue as int = 0
+                    ,@returnmessage as nvarchar(255) = '';
+                
+                exec notiflyer_spEmailConfigureStyle
+                    ,@returnvalue = 0
+                    ,@returnmessage = ''
+
+    @log
+                cc  12122023 - generated basic script file
+*/
+
+
