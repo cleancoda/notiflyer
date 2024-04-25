c134486f2c01a6653167c8a97e6e96b52d0e2811
cleancoda <pranav.shirodkar@protonmail.com>
2024-04-25 18:03:02 -0400

    separated sql agent job step

--- src/mssql/00_setup_ddl_create_objects/00_03_procedures/00_03_41_notiflyer_spSetupSQLAgentJobStep.sql
+++ src/mssql/00_setup_ddl_create_objects/00_03_procedures/00_03_41_notiflyer_spSetupSQLAgentJobStep.sql
@@ -1,21 +1,21 @@
-if object_id('notiflyer_spSetupSQLAgentJob') is not null
-    print 'notiflyer_spSetupSQLAgentJob stored procedure exists, skipping create attempt..'
+if object_id('notiflyer_spSetupSQLAgentJobStep') is not null
+    print 'notiflyer_spSetupSQLAgentJobStep stored procedure exists, skipping create attempt..'
     print 'ignore error below, cannot create exception handling on ddl statements'
     return;
 go
 
--- drop procedure notiflyer_spSetupSQLAgentJob
-create procedure notiflyer_spSetupSQLAgentJob
+-- drop procedure notiflyer_spSetupSQLAgentJobStep
+create procedure notiflyer_spSetupSQLAgentJobStep
 /*
     @author     cleancoda
-    @date       04202024
-    @detail     creates and setup a new sql agent job
+    @date       04252024
+    @detail     creates and setup a new sql agent job step
     @sample
                 declare
                     @returnvalue as int = 0
                     ,@returnmessage as nvarchar(max) = '';
                 
-                exec notiflyer_spSetupSQLAgentJob
+                exec notiflyer_spSetupSQLAgentJobStep
                     @job_id = 2
                     ,@returnvalue = 0
                     ,@returnmessage = ''
@@ -23,7 +23,7 @@ create procedure notiflyer_spSetupSQLAgentJob
                 select @returnvalue, @returnmessage;
 
     @log
-                cc  04202024 - generated basic script file
+                cc  04252024 - generated basic script file
 */
 (
     @job_id as int = 0
@@ -32,27 +32,6 @@ create procedure notiflyer_spSetupSQLAgentJob
 )
 as
 begin
-    /*
-        -- agent needs to be enabled
-        exec sp_configure 
-        -- check if Agent XPs = 1
-
-        -- enable advanced options
-        use master
-        go
-        exec sp_configure 'Show advanced options',1
-        Go
-        reconfigure with override
-        go
-
-        -- set Agent XPs = 1 to enable sql job agent
-        use master
-        go
-        exec sp_configure 'Agent XPs',1
-        Go
-        reconfigure with override
-        go
-    */
 
     declare
         @job_name as nvarchar(255) = 'notiflyer_job'
@@ -62,7 +41,7 @@ begin
         ,@job_step_name as nvarchar(255) = 'notiflyer_job_step'
         ,@job_step_command as nvarchar(255) = 'exec notiflyer_spProcessNotification'
         ,@job_step_subsystem as nvarchar(255) = 'T-SQL'
-        ,@database_name as nvarchar(255) = 'notiflyer'
+        ,@database_name as nvarchar(255) = ''
         ,@active_start_date as int = 0
         ,@active_end_date as int = 99991231
         ,@freq_type as int = 4 -- (h)ourly,@ (d)aily,@ (w)eekly,@ (m)onthly
@@ -157,36 +136,35 @@ begin
         --                     when 'm' then 0
         --                     end
 
-    -- create job
-    if not exists (select 1 from msdb.dbo.sysjobs where name = @job_name)
-    begin
-        -- create job
-        exec msdb.dbo.sp_add_job
+    -- create job step
+        exec msdb.dbo.sp_add_jobstep
             @job_name = @job_name
-            ,@description = @job_description
-            ,@enabled = 1;
-
-        -- create job schedule
-        exec msdb.dbo.sp_add_jobschedule
-            @job_name = @job_name
-            ,@name = @job_name
-            ,@enabled = 1
-            -- (h)ourly, (d)aily, (w)eekly, (m)onthly
-            ,@freq_type = @freq_type
-            ,@freq_interval = @freq_interval
-            ,@freq_subday_type = @freq_subday_type
-            ,@freq_subday_interval = 1
-            ,@freq_relative_interval = 0
-            ,@freq_recurrence_factor = 1
-            ,@active_start_date = @active_start_date
-            ,@active_end_date = @active_end_date
-            ,@active_start_time = 0
-            ,@active_end_time = 235959;
-
-        -- add job to server
-        exec msdb.dbo.sp_add_jobserver
-            @job_name = @job_name;
-    end;
-
+            ,@step_name = @job_step_name
+            ,@step_id = 1
+            ,@command = @job_step_command
+            --,@database_name = @database_name
+            -- ,@subsystem = @job_step_subsystem
+            -- ,@on_success_action = @job_step_on_success_action
+            -- ,@on_fail_action = @job_step_on_fail_action
+            -- ,@retry_attempts = @job_step_retry_attempts
+            -- ,@retry_interval = @job_step_retry_interval
+            -- ,@output_file_name = @job_step_output_file_name
+            -- ,@output_file_append = @job_step_output_file_append
+            -- ,@output_file_overwrite = @job_step_output_file_overwrite
+            -- ,@output_file_no_output = @job_step_output_file_no_output
+            -- ,@output_file_delete = @job_step_output_file_delete
+            -- ,@output_file_reopen = @job_step_output_file_reopen
+            -- ,@output_file_reopen_interval = @job_step_output_file_reopen_interval
+            -- ,@output_file_reopen_threshold = @job_step_output_file_reopen_threshold
+            -- ,@output_file_reopen_action = @job_step_output_file_reopen_action
+            -- ,@output_file_reopen_action_threshold = @job_step_output_file_reopen_action_threshold
+            -- ,@output_file_reopen_action_interval = @job_step_output_file_reopen_action_interval
+            -- ,@output_file_reopen_action_max = @job_step_output_file_reopen_action_max
+            -- ,@output_file_reopen_action_min = @job_step_output_file_reopen_action_min
+            -- ,@output_file_reopen_action_max_interval = @job_step_output_file_reopen_action_max_interval
+            -- ,@output_file_reopen_action_min_interval = @job_step_output_file_reopen_action_min_interval
+            -- ,@output_file_reopen_action_max_threshold = @job_step_output_file_reopen_action_max_threshold
+            -- ,@output_file_reopen_action_min_threshold = @job_step_output_file_reopen_action_min_threshold
+            -- ,@output_file_reopen_action_max_action = @job_step_output_file_reopen_action_max_action;
 
 end
